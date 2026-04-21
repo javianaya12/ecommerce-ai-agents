916e2d98c343f05141fdd6d0b99c298ca7e7a6bc
javianaya12 <janaya@imporey.com>
2026-04-20 18:57:33 -0600

    Create load_data.py

---
+++ data/load_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def load_sales_data():
+    df = pd.read_csv("data/sales.csv")
+
+    ventas_mes = (df["cantidad"] * df["precio"]).sum()
+    productos_vendidos = df["cantidad"].sum()
+
+    producto_top = (
+        df.groupby("producto")["cantidad"]
+        .sum()
+        .idxmax()
+    )
+
+    return {
+        "ventas_mes": ventas_mes,
+        "productos_vendidos": productos_vendidos,
+        "producto_top": producto_top
+    }
